3b37a7aadac27db1b551e4681cbf7fd5e32c434b
ahmedismail-10 <ahmedismaeel064@gmail.com>
2025-11-13 13:20:36 +0200

    Create ServerFailure from dio error

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   HomeRepoImpl(this._apiService);
@@ -18,8 +19,14 @@ class HomeRepoImpl implements HomeRepo {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
+    } on DioException catch (e) {
+      return left(
+        ServerFailure.fromDioException(e),
+      );
     } catch (e) {
-      return left(ServerFailure());
+      return left(
+        ServerFailure(errMessage: 'Unexpecter error: ${e.toString()}'),
+      );
     }
   }
 
