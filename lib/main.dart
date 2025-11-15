e876f95455d962f98c33ad19a6708e6916a19d3d
ahmedismail-10 <ahmedismaeel064@gmail.com>
2025-11-15 15:15:07 +0200

    Set up service locator and implement dependency injection for HomeRepoImpl and ApiService

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setUpServiceLocator();
   runApp(const Bookly());
 }
 
