51b67fe6210d3f7160fb4a23cc0dceb5d6e7f461
ahmedismail-10 <ahmedismaeel064@gmail.com>
2025-11-22 14:42:21 +0200

    display book details

--- lib/features/home/presentation/views/widgets/book_details_section.dart
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -1,35 +1,38 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/views/widgets/books_button_action.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsSection extends StatelessWidget {
-  const BookDetailsSection({super.key});
+  const BookDetailsSection({super.key, required this.bookModel});
 
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         SizedBox(
           height: MediaQuery.of(context).size.height * (243 / 812),
-          child: const BookImage(
-            imageUrl:
-                'http://books.google.com/books/content?id=4MlcEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+          child: BookImage(
+            imageUrl: bookModel.volumeInfo?.imageLinks?.thumbnail ?? '',
           ),
         ),
         const SizedBox(
           height: 42,
         ),
-        const Text(
-          'The Jungle Book',
+        Text(
+          bookModel.volumeInfo?.title ?? 'None',
+          textAlign: TextAlign.center,
           style: Styles.textStyle30,
         ),
         const SizedBox(
           height: 13,
         ),
         Text(
-          'Rudyard Kipling',
+          bookModel.volumeInfo?.authors?[0] ?? 'None',
+          textAlign: TextAlign.center,
           style: Styles.textStyle18.copyWith(
             color: Colors.white.withValues(
               alpha: 0.7,
@@ -40,9 +43,9 @@ class BookDetailsSection extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        const BookRating(
-          rating: 4.8,
-          count: 2390,
+        BookRating(
+          rating: bookModel.volumeInfo?.averageRating ?? 0,
+          count: bookModel.volumeInfo?.ratingsCount ?? 0,
           mainAxisAlignment: MainAxisAlignment.center,
         ),
         const SizedBox(
