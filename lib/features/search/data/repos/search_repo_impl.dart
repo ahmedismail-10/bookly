7f027b6acb9535e78c22f577d12fc4b8dbb36261
ahmedismail-10 <ahmedismaeel064@gmail.com>
2025-11-24 16:57:28 +0200

    Create SearchRepo & implement fetchSearchedBooks int SearchRepoImpl

---
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,35 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/models/book_model/book_model.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/search/data/repos/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  final ApiService _apiService;
+
+  SearchRepoImpl(this._apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchedBooks(
+    String searchQuery,
+  ) async {
+    try {
+      var data = await _apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=$searchQuery',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (e) {
+      return left(
+        ServerFailure.fromDioException(e),
+      );
+    } catch (e) {
+      return left(
+        ServerFailure(errMessage: 'Unexpecter error: ${e.toString()}'),
+      );
+    }
+  }
+}
