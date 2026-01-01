b9a0f04bef2f03ecf01048becba534c4c7e50ad1
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-01 19:56:11 +0200

    Create SearchView and implement navigation to it

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,7 +1,9 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -18,7 +20,9 @@ class CustomAppBar extends StatelessWidget {
         CustomIconButton(
           icon: FontAwesomeIcons.magnifyingGlass,
           size: 25,
-          onPressed: () {},
+          onPressed: () {
+            GoRouter.of(context).push(AppRouter.kSearchView);
+          },
         ),
       ],
     );
