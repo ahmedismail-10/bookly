757e2bd5252b9620a953af2bebefcca08ce23d19
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-06 14:51:48 +0200

    Create Failure file for error handling and add `dartz` for error handling

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookModel>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
 }
