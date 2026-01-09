9fb080e69b2e81597966194b9cda583d6d5e8030
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-09 18:01:53 +0200

    Use CachedNetworkImage for network image to handle errors

--- lib/features/home/presentation/views/widgets/book_image.dart
+++ lib/features/home/presentation/views/widgets/book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class BookImage extends StatelessWidget {
@@ -8,9 +9,12 @@ class BookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 150 / 224,
-      child: Image(
-        image: NetworkImage(
-          imageUrl,
+      child: ClipRRect(
+        borderRadius: .circular(16),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
