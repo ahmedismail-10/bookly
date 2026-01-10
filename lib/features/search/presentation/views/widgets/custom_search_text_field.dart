01dc417ad9ba2c77edd6da3b77bc8e62fe9ad6a5
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-01-10 20:04:47 +0200

    display SearchedResult data

--- lib/features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -1,27 +1,53 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_icon_button.dart';
+import 'package:bookly/features/search/presentation/manager/searched_books_cubit/searched_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomSearchTextField extends StatelessWidget {
+class CustomSearchTextField extends StatefulWidget {
   const CustomSearchTextField({super.key});
 
   @override
+  State<CustomSearchTextField> createState() => _CustomSearchTextFieldState();
+}
+
+class _CustomSearchTextFieldState extends State<CustomSearchTextField> {
+  TextEditingController searchController = .new();
+  @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: searchController,
+      onSubmitted: (value) {
+        search(context, value);
+      },
       decoration: InputDecoration(
         hintText: 'Search',
         hintStyle: Styles.textStyle16,
         suffixIcon: CustomIconButton(
           icon: FontAwesomeIcons.magnifyingGlass,
           size: 25,
-          onPressed: () {},
+          onPressed: () {
+            search(context, searchController.text);
+          },
         ),
         border: buildOutlineInputBorder(),
         enabledBorder: buildOutlineInputBorder(),
         focusedBorder: buildOutlineInputBorder(),
       ),
     );
+  }
+
+  void search(BuildContext context, String value) {
+    BlocProvider.of<SearchedBooksCubit>(
+      context,
+    ).fetchSearchedBooks(searchQuery: value);
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
   }
 
   OutlineInputBorder buildOutlineInputBorder() {
--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,19 +1,45 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loading_indicator.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_item.dart';
+import 'package:bookly/features/search/presentation/manager/searched_books_cubit/searched_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.only(bottom: index == 19 ? 0 : 20),
-          // child: const BookItem(),
-        );
+    return BlocBuilder<SearchedBooksCubit, SearchedBooksState>(
+      builder: (context, state) {
+        if (state is SearchedBooksInitial) {
+          return const Center(
+            child: Text(
+              'Search for books',
+              style: Styles.textStyle20,
+            ),
+          );
+        } else if (state is SearchedBooksSuccess) {
+          return ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  bottom: index == state.books.length - 1 ? 0 : 20,
+                ),
+                child: BookItem(
+                  bookModel: state.books[index],
+                ),
+              );
+            },
+          );
+        } else if (state is SearchedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
       },
     );
   }
